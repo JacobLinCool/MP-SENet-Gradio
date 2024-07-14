963f5a77f5161f1000d2934dc758e1f3ebfd7406
JacobLinCool <jacoblincool@gmail.com>
2024-07-15 05:34:37 +0800

    Revert "feat: return mp3"
    This reverts commit 5f7b87a5fb93ac8b297499598fc44269454790e9.

--- app.py
+++ app.py
@@ -175,7 +175,7 @@ with gr.Blocks() as app:
 
     with gr.Row():
         with gr.Column():
-            output = gr.Audio(label="Processed Audio", format="mp3")
+            output = gr.Audio(label="Processed Audio")
         with gr.Column():
             processed_spec = gr.Plot(label="Processed Spectrogram")
 
